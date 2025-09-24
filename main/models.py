8d5c1eb3848de55b6dd8c5784357c4396c30b478
josiah8344 <josiahsimorangkir@gmail.com>
2025-09-24 11:06:02 +0700

    Tugas 4 Complete

--- main/models.py
+++ main/models.py
@@ -1,5 +1,6 @@
 from time import timezone
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Product(models.Model):
@@ -11,6 +12,7 @@ class Product(models.Model):
     category = models.CharField(max_length=50, default="Uncategorized")
     is_featured = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True) 
 
     
     def __str__(self):
